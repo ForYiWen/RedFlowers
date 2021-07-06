d668a93dcc1c1da21370ffff926b95f2c5fd2005
LueQing <lueqing727@outlook.com>
2021-07-06 20:20:39 +0800

    awa

---
+++ README.md
@@ -0,0 +1,5 @@
+# WallPaper-FromInternet
+
+copyed from:
+https://www.jianshu.com/p/7c4f58ee8972
+https://blog.csdn.net/qq_35425276/article/details/79423355
---
+++ js/snow.js
@@ -0,0 +1,68 @@
+const NUMBER_OF_LEAVES = 100;
+
+function init() {
+
+    var container = document.getElementById('leafContainer');
+    for (var i = 0; i < NUMBER_OF_LEAVES; i++) {
+        container.appendChild(createALeaf());
+    }
+}
+
+function randomInteger(low, high) {
+    return low + Math.floor(Math.random() * (high - low));
+}
+
+
+function randomFloat(low, high) {
+    return low + Math.random() * (high - low);
+}
+
+
+function pixelValue(value) {
+    return value + 'px';
+}
+
+function durationValue(value) {
+    return value + 's';
+}
+
+function createALeaf() {
+
+    var leafDiv = document.createElement('div');
+    var image = document.createElement('img');
+
+//需要图片
+    // image.src = './images/' + 'flower' + randomInteger(1, 5) + '.png';
+    image.src = './images/' + 'flower.png';
+
+
+    leafDiv.style.top = "-100px";
+
+
+    leafDiv.style.left = pixelValue(randomInteger(0, window.screen.width));
+
+
+    var spinAnimationName = (Math.random() < 0.5) ? 'clockwiseSpin' : 'counterclockwiseSpinAndFlip';
+
+
+    leafDiv.style.webkitAnimationName = 'fade, drop';
+    image.style.webkitAnimationName = spinAnimationName;
+
+
+    var fadeAndDropDuration = durationValue(randomFloat(5, 11));
+
+    var spinDuration = durationValue(randomFloat(4, 8));
+
+    leafDiv.style.webkitAnimationDuration = fadeAndDropDuration + ', ' + fadeAndDropDuration;
+
+    var leafDelay = durationValue(randomFloat(0, 5));
+    leafDiv.style.webkitAnimationDelay = leafDelay + ', ' + leafDelay;
+
+    image.style.webkitAnimationDuration = spinDuration;
+
+
+    leafDiv.appendChild(image);
+
+    return leafDiv;
+}
+window.addEventListener('load', init, false);
